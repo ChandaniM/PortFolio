010e4bd996eafb04dc1dcbdeef90fd421ef6d354
Chandani Mourya <chandanimourya5@gmail.com>
2021-07-30 21:44:39 +0530

    Some edit done

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ window.onload=()=>{
 }
 
 
- var a="Chandani Mourya";
-var i=0;
-function type()
-{
-	// for(int d=0; d<=Infinity;d++){
-document.getElementById("cursive").innerHTML
-+=a.charAt(i);
-    i++;
-    setTimeout(type,150)
-}
+//  var a="Chandani Mourya";
+// var i=0;
+// function type()
+// {
+// 	// for(int d=0; d<=Infinity;d++){
+// document.getElementById("cursive").innerHTML
+// +=a.charAt(i);
+//     i++;
+//     setTimeout(type,150)
 // }
-type();
+// // }
+// type();
