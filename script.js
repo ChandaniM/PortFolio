4f45c88a98678922cf3dd9f0c7ad356fdbfd5540
Chandani Mourya <chandanimourya5@gmail.com>
2021-07-30 11:04:21 +0530

    everything done

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 window.onload=()=>{
 	AOS.init();
 }
+
+
+ var a="Chandani Mourya";
+var i=0;
+function type()
+{
+	// for(int d=0; d<=Infinity;d++){
+document.getElementById("cursive").innerHTML
++=a.charAt(i);
+    i++;
+    setTimeout(type,150)
+}
+// }
+type();
