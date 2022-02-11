192b05f25aeb2a30a0f423e6cc571fb9283f080d
Chandani Mourya <chandanimourya5@gmail.com>
2022-02-11 22:33:56 +0530

    Update Project Part

---
+++ projects.js
@@ -0,0 +1,130 @@
+	// {
+	// 	'id':'',
+	// 	'Project_Title':'',
+	// 	'Project_Description':'',
+	// 	'Project_Image':'',
+	// 	'Project_Url':''
+	//  }//,
+	// {
+	// 	'id':'',
+	// 	'Project_Title':'',
+	// 	'Project_Description':'',
+	// 	'Project_Image':'',
+	// 	'Project_Url':''
+	// }
+let proj={
+	row1:[
+		{
+		'id':'0',
+		'project_Title':'Chat-Bot',
+		'project_Description':'A Chat-Bot developed using Node.js & Express as part of my internship at  Cloud Counselage.Pvt.Ltd',
+		'project_Image_alt':'This is a ChatBot project & here is the ChatBot Images.',
+		'project_tag':'Completed',
+		'project_Image':'./Images/Chat-bot.png',
+		'project_Url':'https://github.com/ChandaniM/ChatBot'
+	},
+	{
+		'id':'1',
+		'project_Title':'Restaurant Management System',
+		'project_Description':'A full-fledged website developed using HTML,CSS,JS  for front-end and PHP for backend with MYSQL database',
+		'project_Image_alt':'This is a Restaurant Management System project & here is the Restaurant Management System Images.',
+		'project_tag':'Completed',
+		'project_Image':'./Images/intpproect.png',
+		'project_Url':'https://github.com/ChandaniM/INTP_PROJECT_17'
+	},
+	{
+		'id':'2',
+		'project_Title':'React Gallery',
+		'project_Description':'A React-gallery website developed using React. ',
+		'project_Image_alt':'This is a React-gallery project.',
+		'project_tag':'Working',
+		'project_Image':'./Images/ReactGallery.png',
+		'project_Url':'https://github.com/ChandaniM/React-Gallery'
+	}],
+	row2:[
+		{
+		'id':'0',
+		'project_Title':'TODO List',
+		'project_Description':'A TODO List Using React',
+		'project_Image_alt':'This is a TODO List project',
+		'project_tag':'Working',
+		'project_Image':'./Images/ToDo_List.png',
+		'project_Url':'https://github.com/ChandaniM/ToDo_List',
+	},
+		{
+		'id':'1',
+		'project_Title':'Instagram Clone',
+		'project_Description':'UI clone of the social media app Instagram developed using HTML, CSS, Javascript. Currently supported for phones.',
+		'project_Image_alt':'This is a Instagram Clone project & here is the Instagram Clone Images.',
+		'project_tag':'Halted',
+		'project_Image':'./Images/insta-colne.png',
+		'project_Url':'https://github.com/ChandaniM/Instagram-Clone'
+	}
+	]
+}
+
+// export default projects;
+let index=0;
+for(let i in proj){
+	// console.log(proj[i][index])
+	for(let j=0;j<3;j++){
+		let project_card = document.getElementsByClassName("project_container");
+		let div=document.createElement('div');
+		div.classList.add('card','box_card');
+		div.setAttribute('data-aos','fade-down');
+		div.setAttribute('data-aos-duration','2000');
+
+		let image_container=document.createElement('div');
+		image_container.setAttribute('class','image_container');
+
+		let tag_div=document.createElement('p');
+		tag_div.classList.add('tags');	
+		tag_div.innerHTML=proj[i][j].project_tag;
+		if(proj[i][j].project_tag=="Halted"){
+			tag_div.style.backgroundColor="red";
+		}
+		if(proj[i][j].project_tag=="Working"){
+			tag_div.style.backgroundColor="#FFD700";
+		}
+		image_container.appendChild(tag_div);
+
+
+		let image=document.createElement('img');
+		image.classList.add('Image','card-img-top');
+		image.setAttribute('src',proj[i][j].project_Image);
+		image_container.appendChild(image);
+		// div.appendChild(image);\
+		div.appendChild(image_container);
+
+		let card_detail_container=document.createElement('div');
+		card_detail_container.setAttribute('class','card-body');
+		let card_title=document.createElement('h5');
+		card_title.setAttribute('class','card_title');
+		card_title.innerHTML=proj[i][j].project_Title;
+		card_detail_container.appendChild(card_title);
+
+		let card_Description=document.createElement('p');
+		card_Description.innerHTML=proj[i][j].project_Description;
+		card_Description.classList.add('description');
+		card_detail_container.appendChild(card_Description);
+
+		let button_link=document.createElement('a');
+		button_link.classList.add('btn','btn-primary','small-font');
+		button_link.setAttribute('href',proj[i][j].project_Url);
+		button_link.innerHTML="View Project";
+
+		card_detail_container.appendChild(button_link);
+		div.appendChild(card_detail_container);
+		project_card[0].appendChild(div);
+		// console.log(proj[i][j].Project_Title)
+	}
+}
+
+// for(let i in proj){
+// 	// console.log(proj[i][index])
+// 	for(let j=0;j<3;j++){
+// 		if(proj[i][j].project_tag=="Halted"){
+// 			document.getElementsByClassName('tags').style.backgroundColor = "red";
+// 		}
+// 	}
+// }
--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.onload=()=>{
 	AOS.init();
 }
 
-
 //  var a="Chandani Mourya";
 // var i=0;
 // function type()
@@ -15,3 +14,122 @@ window.onload=()=>{
 // }
 // // }
 // type();
+// Sets age automatically
+
+// const age=()=>{
+	// Get Current Date
+// 	let date=new Date();
+// 	let today_date=date.getDate();
+// 	let month=date.getMonth()+1;
+// 	let year=date.getFullYear();
+// 	//Check if birthdate has already passed 
+// 	if(today_date>=12 && month>=12){
+// 		// Today's date>15 
+// 			//month after october(so age= year-born year)
+			
+// 			document.getElementsByClassName('age')[0].innerText=`${year-2000} Years`;	
+// 			// document.getElementsByClassName('age')[1].innerText=`${year-2000} Years`;
+
+// 	}else{
+// 		if(month>12){
+// 			document.getElementsByClassName('age')[0].innerText=`${year-2000} Years`;
+// 			// document.getElementsByClassName('age')[1].innerText=`${year-2000} Years`;		
+
+
+// 		}else{
+// 			document.getElementsByClassName('age')[0].innerText=`${year-2000-1} Years`;
+// 			// document.getElementsByClassName('age')[1].innerText=`${year-2000-1} Years`;
+			
+
+// 		}
+// 	}
+// }
+
+// Add Skills
+const skill=[
+		{
+		'id':'0',
+		'image_url':"./logo/icons8-react-native.svg",
+		'alt':'React'
+		}//,
+// 		{
+// 		'id':'1',
+// 		'image_url':"./logo/js.svg",
+// 		'alt':'Javascript'
+// 		},
+// 		{
+// 		'id':'2',
+// 		'image_url':"./logo/html-5-01.svg",
+// 		'alt':'HTML'
+// 		},
+// 		{
+// 		'id':'3',
+// 		'image_url':"./logo/css3-alter.svg",
+// 		'alt':'CSS'
+// 		},
+// 		{
+// 		'id':'4',
+// 		'image_url':"./logo/Bootstrap.svg",
+// 		'alt':'BootStrap'
+// 		},
+// 		{
+// 		'id':'5',
+// 		'image_url':"./logo/node.svg",
+// 		'alt':'Node.js'
+// 		},
+// 		{
+// 		'id':'6',
+// 		'image_url':"./logo/mongodb-alt.svg",
+// 		'alt':'Mango DB'
+// 		},
+// 		{
+// 		'id':'7',
+// 		'image_url':"./logo/git.svg",
+// 		'alt':'Git'
+// 		},
+// 		{
+// 		'id':'8',
+// 		'image_url':"./logo/phython.svg",
+// 		'alt':'phython'
+// 		},
+// 		{
+// 		'id':'9',
+// 		'image_url':"./logo/java-alt.svg",
+// 		'alt':'Java'
+// 		},
+// 		// {
+// 		// 'id':'10',
+// 		// 'image_url':"",
+// 		// 'alt':'React'
+// 		// },
+// 		{
+// 		'id':'10',
+// 		'image_url':"./logo/sqlserver.svg",
+// 		'alt':'SQL'
+// 		},
+// 		{
+// 		'id':'11',
+// 		'image_url':"./logo/php-edit.svg",
+// 		'alt':'PHP'
+// 		},
+// 		{
+// 		'id':'12',
+// 		'image_url':"./logo/less-seeklogo.com.svg",
+// 		'alt':'LESS'
+// 		},
+// 		{
+// 		'id':'13',
+// 		'image_url':"./logo/sass.svg",
+// 		'alt':'SASS'
+// 		},
+// 		{
+// 		'id':'14',
+// 		'image_url':"./logo/icons8-firebase.svg",
+// 		'alt':'FireBase'
+// 		},
+// 		{
+// 		'id':'15',
+// 		'image_url':"./logo/figma-alter.svg",
+// 		'alt':'Figma'
+// 		}
+];
