c425fe4d0c87a88d03c56608e8d6c53c79a56b60
ChandaniM <chandanimourya5@gmail.com>
2025-09-14 18:25:20 +0530

    - Updating my portfolio

--- projects.js
+++ projects.js
@@ -1,125 +1,97 @@
-let proj={
-	row1:[
-		{
-		'id':'0',
-		'project_Title':'Chat-Bot',
-		'project_Description':'A Chat-Bot developed using Node.js & Express as part of my internship at  Cloud Counselage.Pvt.Ltd',
-		'project_Image_alt':'This is a ChatBot project & here is the ChatBot Images.',
-		'project_tag':'Completed',
-		'project_Image':'./Images/Chat-bot.png',
-		'project_Url':'https://github.com/ChandaniM/ChatBot',
-		'project_Message':'Chat-Bot'
-
-	},
-	{
-		'id':'1',
-		'project_Title':'E-Commerce Web application',
-		'project_Description':'A full-stack E-Commerce web application that has both the customer and seller sides where customer can buy products and seller can sell their products on the website.',
-		'project_Image_alt':'This is a E-Commerce Website.',
-		'project_tag':'Completed',
-		'project_Image':'./Images/Ecommerce.png',
-		'project_Url':'https://github.com/ChandaniM/E-commerce-Application',
-		'project_Message':'E-Commerce Website'
-	},
-	{
-		'id':'2',
-		'project_Title':'Restaurant Management System',
-		'project_Description':'A full-fledged website developed using HTML,CSS,JS  for front-end and PHP for backend with MYSQL database',
-		'project_Image_alt':'This is a Restaurant Management System project & here is the Restaurant Management System Images.',
-		'project_tag':'Completed',
-		'project_Image':'./Images/intpproect.png',
-		'project_Url':'https://github.com/ChandaniM/INTP_PROJECT_17',
-		'project_Message':'Restaurant Management System'
-	}],
-	row2:[
-	{
-		'id':'0',
-		'project_Title':'React Gallery',
-		'project_Description':'A React-gallery website developed using React. ',
-		'project_Image_alt':'This is a React-gallery project.',
-		'project_tag':'Working',
-		'project_Image':'./Images/ReactGallery.png',
-		'project_Url':'https://github.com/ChandaniM/React-Gallery',
-		'project_Message':'React Gallery'
-	},
-		{
-		'id':'1',
-		'project_Title':'TODO List',
-		'project_Description':'A TODO List Using React',
-		'project_Image_alt':'This is a TODO List project',
-		'project_tag':'Working',
-		'project_Image':'./Images/TODO_List.png',
-		'project_Url':'https://github.com/ChandaniM/ToDo_List',
-		'project_Message':'TODO List'
-	},
-		{
-		'id':'2',
-		'project_Title':'Instagram Clone',
-		'project_Description':'UI clone of the social media app Instagram developed using HTML, CSS, Javascript. Currently supported for phones.',
-		'project_Image_alt':'This is a Instagram Clone project & here is the Instagram Clone Images.',
-		'project_tag':'Halted',
-		'project_Image':'./Images/insta-colne.png',
-		'project_Url':'https://github.com/ChandaniM/Instagram-Clone',
-		'project_Message':'Instagram Clone'
-	}
-	]
-}
+let proj = {
+  row1: [
+    {
+      id: "0",
+      project_Title: "Restaurant Management System",
+      project_Description:
+        "A full-fledged website developed using HTML,CSS,JS  for front-end and PHP for backend with MYSQL database",
+      project_Image_alt:
+        "This is a Restaurant Management System project & here is the Restaurant Management System Images.",
+      project_tag: "Completed",
+      project_Image: "./Images/intpproect.png",
+      project_Url: "https://github.com/ChandaniM/INTP_PROJECT_17",
+      project_Message: "Restaurant Management System",
+    },
+    {
+      id: "1",
+      project_Title: "Chat-Bot",
+      project_Description:
+        "A Chat-Bot developed using Node.js & Express as part of my internship at  Cloud Counselage.Pvt.Ltd",
+      project_Image_alt:
+        "This is a ChatBot project & here is the ChatBot Images.",
+      project_tag: "Completed",
+      project_Image: "./Images/Chat-bot.png",
+      project_Url: "https://github.com/ChandaniM/ChatBot",
+      project_Message: "Chat-Bot",
+    },
+    {
+      id: "2",
+      project_Title: "E-Commerce Web application",
+      project_Description:
+        "A full-stack E-Commerce web application that has both the customer and seller sides where customer can buy products and seller can sell their products on the website.",
+      project_Image_alt: "This is a E-Commerce Website.",
+      project_tag: "Completed",
+      project_Image: "./Images/Ecommerce.png",
+      project_Url: "https://github.com/ChandaniM/E-commerce-Application",
+      project_Message: "E-Commerce Website",
+    },
+  ],
+};
 
 // export default projects;
-let index=0;
-for(let i in proj){
-	// console.log(proj[i][index])
-	for(let j=0;j<proj[i].length;j++){
-		let project_card = document.getElementsByClassName("project_container");
-		let div=document.createElement('div');
-		div.classList.add('card','box_card');
-		div.setAttribute('data-aos','fade-down');
-		div.setAttribute('data-aos-duration','2000');
-		// div.setAttribute('data-toggle','tooltip')
-		// div.setAttribute('data-placement','right');
-		div.setAttribute('title',proj[i][j].project_Message);
-		let image_container=document.createElement('div');
-		image_container.setAttribute('class','image_container');
+let index = 0;
+for (let i in proj) {
+  // console.log(proj[i][index])
+  for (let j = 0; j < proj[i].length; j++) {
+    let project_card = document.getElementsByClassName("project_container");
+    let div = document.createElement("div");
+    div.classList.add("card", "box_card");
+    div.setAttribute("data-aos", "fade-down");
+    div.setAttribute("data-aos-duration", "2000");
+    // div.setAttribute('data-toggle','tooltip')
+    // div.setAttribute('data-placement','right');
+    div.setAttribute("title", proj[i][j].project_Message);
+    let image_container = document.createElement("div");
+    image_container.setAttribute("class", "image_container");
 
-		let tag_div=document.createElement('p');
-		tag_div.classList.add('tags');	
-		tag_div.innerHTML=proj[i][j].project_tag;
-		if(proj[i][j].project_tag=="Halted"){
-			tag_div.style.backgroundColor="red";
-		}
-		if(proj[i][j].project_tag=="Working"){
-			tag_div.style.backgroundColor="#FFD700";
-		}
-		image_container.appendChild(tag_div);
+    let tag_div = document.createElement("p");
+    tag_div.classList.add("tags");
+    tag_div.innerHTML = proj[i][j].project_tag;
+    if (proj[i][j].project_tag == "Halted") {
+      tag_div.style.backgroundColor = "red";
+    }
+    if (proj[i][j].project_tag == "Working") {
+      tag_div.style.backgroundColor = "#FFD700";
+    }
+    image_container.appendChild(tag_div);
 
+    let image = document.createElement("img");
+    image.classList.add("Image", "card-img-top");
+    image.setAttribute("src", proj[i][j].project_Image);
+    image_container.appendChild(image);
+    // div.appendChild(image);\
+    div.appendChild(image_container);
 
-		let image=document.createElement('img');
-		image.classList.add('Image','card-img-top');
-		image.setAttribute('src',proj[i][j].project_Image);
-		image_container.appendChild(image);
-		// div.appendChild(image);\
-		div.appendChild(image_container);
+    let card_detail_container = document.createElement("div");
+    card_detail_container.setAttribute("class", "card-body");
+    let card_title = document.createElement("h5");
+    card_title.setAttribute("class", "card_title");
+    card_title.innerHTML = proj[i][j].project_Title;
+    card_detail_container.appendChild(card_title);
 
-		let card_detail_container=document.createElement('div');
-		card_detail_container.setAttribute('class','card-body');
-		let card_title=document.createElement('h5');
-		card_title.setAttribute('class','card_title');
-		card_title.innerHTML=proj[i][j].project_Title;
-		card_detail_container.appendChild(card_title);
+    let card_Description = document.createElement("p");
+    card_Description.innerHTML = proj[i][j].project_Description;
+    card_Description.classList.add("description");
+    card_detail_container.appendChild(card_Description);
 
-		let card_Description=document.createElement('p');
-		card_Description.innerHTML=proj[i][j].project_Description;
-		card_Description.classList.add('description');
-		card_detail_container.appendChild(card_Description);
+    let button_link = document.createElement("a");
+    button_link.classList.add("btn", "btn-primary", "small-font");
+    button_link.setAttribute("href", proj[i][j].project_Url);
+    button_link.innerHTML = "View Project";
 
-		let button_link=document.createElement('a');
-		button_link.classList.add('btn','btn-primary','small-font');
-		button_link.setAttribute('href',proj[i][j].project_Url);
-		button_link.innerHTML="View Project";
-
-		card_detail_container.appendChild(button_link);
-		div.appendChild(card_detail_container);
-		project_card[0].appendChild(div);
-		// console.log(proj[i][j].Project_Title)
-	}
+    card_detail_container.appendChild(button_link);
+    div.appendChild(card_detail_container);
+    project_card[0].appendChild(div);
+    // console.log(proj[i][j].Project_Title)
+  }
 }
--- script.js
+++ script.js
@@ -1,162 +1,224 @@
-const age=()=>{
-	// Get Current Date
-	let date=new Date();
-	let today_date=date.getDate();
-	let month=date.getMonth()+1;
-	let year=date.getFullYear();
-	//Check if birthdate has already passed 
-	let ageDiv=document.getElementsByClassName('age');
-	console.log(ageDiv)
-	if(today_date>=12 && month>=12){
-		// Today's date>12
-			//month after october(so age= year-born year)
-		ageDiv[0].innerText=`${year-2000} Years`;
-		ageDiv[1].innerText=`${year-2000} Years`;
+let totalYearOfExperience = document.getElementById("total-year-of-experience");
+let experience = document.getElementById("experience");
 
-	}
-	else{
-		if(month>12){
-			ageDiv[0].innerText=`${year-2000} Years`;
-			ageDiv[1].innerText=`${year-2000} Years`;
-		}
-		else{
-			ageDiv[0].innerText=`${year-2000-1} Years`;
-			ageDiv[1].innerText=`${year-2000-1} Years`;
-		}
-	}
-}
+// const age = () => {
+//   const today = new Date();
+//   const birthYear = 2000;
+//   const birthMonth = 12;
+//   const birthDay = 12;
 
+//   let currentAge = today.getFullYear() - birthYear;
+
+//   if (
+//     today.getMonth() + 1 < birthMonth ||
+//     (today.getMonth() + 1 === birthMonth && today.getDate() < birthDay)
+//   ) {
+//     currentAge--;
+//   }
+
+//   // Update DOM
+//   const ageElements = document.getElementsByClassName("age");
+//   for (let i = 0; i < ageElements.length; i++) {
+//     ageElements[i].innerText = `${currentAge} Years`;
+//   }
+// };
 
 // Add Skills
-const skill=[
-		{
-		'id':'0',
-		'image_url':"./logo/icons8-react-native.svg",
-		'alt':'React',
-		'title':'React'
-		},
-		{
-		'id':'1',
-		'image_url':"./logo/js.svg",
-		'alt':'Javascript',
-		'title':'Javascript'
-		},
-		{
-		'id':'2',
-		'image_url':"./logo/html-5-01.svg",
-		'alt':'HTML',
-		'title':'HTML'
-		},
-		{
-		'id':'3',
-		'image_url':"./logo/css3-alter.svg",
-		'alt':'CSS',
-		'title':'CSS'
-		},
-		{
-		'id':'4',
-		'image_url':"./logo/Bootstrap.svg",
-		'alt':'BootStrap',
-		'title':'BootStrap'
-		},
-		{
-		'id':'5',
-		'image_url':"./logo/node.svg",
-		'alt':'Node.js',
-		'title':'Node.js'
-		},
-		{
-		'id':'6',
-		'image_url':"./logo/mongodb-alt.svg",
-		'alt':'Mongo DB',
-		'title':'Mongo DB'
-		},
-		{
-		'id':'7',
-		'image_url':"./logo/git.svg",
-		'alt':'Git',
-		'title':'Git'
-		},
-		{
-		'id':'8',
-		'image_url':"./logo/phython.svg",
-		'alt':'Python',
-		'title':'Python'
-		},
-		{
-		'id':'9',
-		'image_url':"./logo/java-alt.svg",
-		'alt':'Java',
-		'title':'Java'
-		},
-		// {
-		// 'id':'10',
-		// 'image_url':"",
-		// 'alt':'React'
-		// },
-		{
-		'id':'10',
-		'image_url':"./logo/sqlserver.svg",
-		'alt':'SQL',
-		'title':'SQL'
-		},
-		{
-		'id':'11',
-		'image_url':"./logo/php-edit.svg",
-		'alt':'PHP',
-		'title':'PHP'
-		},
-		{
-		'id':'12',
-		'image_url':"./logo/less-seeklogo.com.svg",
-		'alt':'LESS',
-		'title':'LESS'
-		},
-		{
-		'id':'13',
-		'image_url':"./logo/sass.svg",
-		'alt':'SASS',
-		'title':'SASS'
-		},
-		{
-		'id':'14',
-		'image_url':"./logo/icons8-firebase.svg",
-		'alt':'FireBase',
-		'title':'FireBase'
-		},
-		{
-		'id':'15',
-		'image_url':"./logo/figma-alter.svg",
-		'alt':'Figma',
-		'title':'Figma'
-		}
+const skill = [
+  // Frontend frameworks and languages
+  {
+    id: "1",
+    image_url: "./logo/angular.png",
+    alt: "Angular JS",
+    title: "Angular JS",
+  },
+  {
+    id: "2",
+    image_url: "./logo/icons8-react-native.svg",
+    alt: "React",
+    title: "React",
+  },
+  {
+    id: "3",
+    image_url: "./logo/Typescript.png",
+    alt: "TypeScript",
+    title: "TypeScript",
+  },
+  {
+    id: "4",
+    image_url: "./logo/js.svg",
+    alt: "Javascript",
+    title: "Javascript",
+  },
+  {
+    id: "5",
+    image_url: "./logo/html-5-01.svg",
+    alt: "HTML",
+    title: "HTML",
+  },
+
+  // Styling tools
+  {
+    id: "6",
+    image_url: "./logo/css3-alter.svg",
+    alt: "CSS",
+    title: "CSS",
+  },
+  {
+    id: "7",
+    image_url: "./logo/Bootstrap.svg",
+    alt: "BootStrap",
+    title: "BootStrap",
+  },
+  {
+    id: "8",
+    image_url: "./logo/Tailwindcss.png",
+    alt: "Tailwind",
+    title: "Tailwind",
+  },
+  {
+    id: "9",
+    image_url: "./logo/less-seeklogo.com.svg",
+    alt: "LESS",
+    title: "LESS",
+  },
+  {
+    id: "10",
+    image_url: "./logo/sass.svg",
+    alt: "SASS",
+    title: "SASS",
+  },
+
+  // Backend and databases
+  {
+    id: "11",
+    image_url: "./logo/node.svg",
+    alt: "Node.js",
+    title: "Node.js",
+  },
+  {
+    id: "12",
+    image_url: "./logo/phython.svg",
+    alt: "Python",
+    title: "Python",
+  },
+  {
+    id: "13",
+    image_url: "./logo/Docker.png",
+    alt: "Docker",
+    title: "Docker",
+  },
+  {
+    id: "14",
+    image_url: "./logo/mongodb-alt.svg",
+    alt: "Mongo DB",
+    title: "Mongo DB",
+  },
+  {
+    id: "15",
+    image_url: "./logo/sqlserver.svg",
+    alt: "SQL",
+    title: "SQL",
+  },
+  {
+    id: "16",
+    image_url: "./logo/icons8-firebase.svg",
+    alt: "Firebase",
+    title: "Firebase",
+  },
+
+  // Developer tools and others
+  {
+    id: "17",
+    image_url: "./logo/git.svg",
+    alt: "Git",
+    title: "Git",
+  },
+  {
+    id: "18",
+    image_url: "./logo/figma-alter.svg",
+    alt: "Figma",
+    title: "Figma",
+  },
+  {
+    id: "19",
+    image_url: "./logo/FlutterFlow.png",
+    alt: "FlutterFlow",
+    title: "FlutterFlow",
+  },
+  {
+    id: "20",
+    image_url: "./logo/Grafana.jpg",
+    alt: "Grafana",
+    title: "Grafana",
+  },
+  {
+    id: "21",
+    image_url: "./logo/M-files.png",
+    alt: "M-files",
+    title: "M-files",
+  },
 ];
-// const display_skill=()=>{
-		for(let i of skill){
-			let skill_container = document.getElementsByClassName("skill_container");
-			let skill_card= document.createElement('div');
-			skill_card.classList.add( 'card','skill_card');
-			skill_card.setAttribute('data-toggle','tooltip');
-			skill_card.setAttribute('data-placement','right');
-			skill_card.setAttribute('title',i.title);
-			let skill_image= document.createElement('img');
-			skill_image.classList.add('skill_image','image');
-			skill_image.setAttribute('src',i.image_url);
-			skill_image.setAttribute('alt',i.alt);
-			// 'data-toggle':"tooltip",
-			// 'data-placement':"bottom"
 
-			// i.image_url 
-			skill_card.appendChild(skill_image);
-			skill_container[0].appendChild(skill_card);
-		}
-// }
-
-window.onload=()=>{
-	AOS.init();
-	age();
-	// display_skill();
+for (let i of skill) {
+  let skill_container = document.getElementsByClassName("skill_container");
+  let skill_card = document.createElement("div");
+  skill_card.classList.add("card", "skill_card");
+  skill_card.setAttribute("data-toggle", "tooltip");
+  skill_card.setAttribute("data-placement", "right");
+  skill_card.setAttribute("title", i.title);
+  let skill_image = document.createElement("img");
+  skill_image.classList.add("skill_image", "image");
+  skill_image.setAttribute("src", i.image_url);
+  skill_image.setAttribute("alt", i.alt);
+  skill_card.appendChild(skill_image);
+  skill_container[0].appendChild(skill_card);
 }
+// }
+function calculateExperience(dateOfJoining) {
+  const joinDate = new Date(dateOfJoining);
+  const today = new Date();
+
+  let years = today.getFullYear() - joinDate.getFullYear();
+  let months = today.getMonth() - joinDate.getMonth();
+
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
+  return `${years} years ${months} months`;
+}
+
+const skillSets = document.querySelectorAll(".skill-set");
+
+skillSets.forEach((skillSet) => {
+  const toggleBtn = skillSet.querySelector(".toggle-btn");
+  const hiddenSkills = skillSet.querySelector(".hide-skills");
+
+  const hiddenSkillsText = hiddenSkills.textContent.trim();
+  const hiddenSkillsCount = hiddenSkillsText
+    .split(",")
+    .filter((s) => s.trim().length > 0).length;
+  toggleBtn.textContent = `+${hiddenSkillsCount} skills`;
+
+  toggleBtn.addEventListener("click", () => {
+    if (hiddenSkills.style.display === "none" || !hiddenSkills.style.display) {
+      hiddenSkills.style.display = "inline"; // Show
+      toggleBtn.textContent = "Show less";
+    } else {
+      hiddenSkills.style.display = "none"; // Hide
+      toggleBtn.textContent = `+${hiddenSkillsCount} skills`;
+    }
+  });
+});
+
+window.onload = () => {
+  totalYearOfExperience.innerText = calculateExperience("22-August-2022");
+  experience.innerText = calculateExperience("22-August-2022");
+  // age();
+  AOS.init();
+};
 $(function () {
-$('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
